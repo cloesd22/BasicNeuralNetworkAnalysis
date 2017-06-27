2e78e410ca51bea2c0f26bc0b53678454d32652c
cloesd22 <cloesd22@gmail.com>
2017-06-27 19:06:01 +1000

    Added Neural network cross validation

--- Ann.py
+++ Ann.py
@@ -17,7 +17,8 @@ import pandas as pd
 #Import data set up X and Y variables
 dataset = pd.read_csv('sampledata.csv',encoding = "ISO-8859-1")
 X = dataset.iloc[:,2:7]
-Y = dataset.iloc[:,7]
+Y = dataset.iloc[:,8]
+
 
 #no need to use label encoder as all variables are numeric
 #no need to use 1 hot encoder as all varaibles are numeric ((No need for 1ofK))
@@ -32,10 +33,8 @@ X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.2,random_state=
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 sc_Y = StandardScaler()
-X_train=sc.fit_transform(X_train)
-X_test=sc.transform(X_test)
-Y_train=sc_Y.fit_transform(Y_train)
-Y_test=sc_Y.fit(Y_train)
+X_train=sc_X.fit_transform(X_train)
+X_test=sc_X.transform(X_test)
 
 #Data pre-processing=====================================================
 
@@ -45,17 +44,29 @@ Y_test=sc_Y.fit(Y_train)
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
 
-classifier = Sequential()
 
-classifier.add(Dense(output_dim=4,init='uniform',activation='relu',input_dim=5))
-classifier.add(Dense(output_dim=4,init='uniform',activation='relu'))
-classifier.add(Dense(output_dim=1,init='uniform',activation='sigmoid'))
-
-classifier.compile(optimizer='rmsprop',loss='binary_crossentropy',metrics=['accuracy'])
-classifier.fit(X_train,Y_train,batch_size=10, epochs=500)
+def build_classifier():
+    classifier = Sequential()
+    classifier.add(Dense(output_dim=3,init='uniform',activation='relu',input_dim=5))
+    classifier.add(Dense(output_dim=3,init='uniform',activation='relu'))
+    classifier.add(Dense(output_dim=1,init='uniform',activation='sigmoid'))
+    classifier.compile(optimizer='adam',loss='binary_crossentropy', metrics=['accuracy'])
+    return classifier
 
 
 #Building ANN Structure==================================================
-for layer in classifier.layers:
-    weights=layer.get_weights()
+
+#Cross-Validation==================================================
+
+classifier = KerasClassifier(build_fn=build_classifier, batch_size=10, nb_epoch=100)
+accuracies = cross_val_score(estimator=classifier,X=X_train,y=Y_train,cv=10,n_jobs=1)
+#Remmeber, n_jobs =1, if <1 or >1 wit will fail.
+mean=accuracies.mean()
+variance = accuracies.std()
+
+
+#Cross-Validation==================================================
+
