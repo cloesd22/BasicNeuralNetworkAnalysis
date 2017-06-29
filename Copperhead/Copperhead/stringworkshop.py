c112ae81c6ce09f8507919b734bd20ee917faf52
cloesd22 <cloesd22@gmail.com>
2017-06-29 13:47:08 +1000

    scraper with beautiful soup at a working state

---
+++ Copperhead/Copperhead/stringworkshop.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 29 04:23:59 2017
+
+@author: manoj
+"""
+
+##String Processor, capable of doing advanced mass string functions such as
+#word count, word frequency calculation etc.
+
+
+class stringworkshop():
+    
+    def wordcount(text):
+        #simple word count, input text, returns integer wordcount
+        wordcount = text.split()
+        wordcount = (len(wordcount))
+        return wordcount
+    
+    def wordfreq(word,text):
+        #simple word frequency, provide text, and word returns frequency integer.
+        count = 0
+        
+        textarray = text.split()
+        
+        for unit in textarray:
+            if word ==unit:
+                count=count+1
+        
+        return count
+        
+        
