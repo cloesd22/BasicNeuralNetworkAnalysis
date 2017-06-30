617a8c8abba7434a2487dbf400b8c92ceb49615e
cloesd22 <cloesd22@gmail.com>
2017-06-30 19:44:53 +1000

    Completed base version, added review file

--- AnnCopperhead.py
+++ AnnCopperhead.py
@@ -20,7 +20,8 @@ X = dataset.iloc[:,2:7]
 Y = dataset.iloc[:,8]
 
 from sklearn.model_selection import train_test_split
-X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.1,random_state=1)
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.1,random_state=0)
+#SET TEST_SIZE TO 0 TO TURN ENTIRE DATASET INTO TRAINING SET FOR K-CROSSVALIDATION
 
 
 from sklearn.preprocessing import StandardScaler
@@ -28,8 +29,8 @@ scX = StandardScaler()
 scY = StandardScaler()
 X_train = scX.fit_transform(X_train)
 X_test = scX.transform(X_test)
-#Y_train = scY.fit_transform(Y_train)
-#Y_test = scY.transform(Y_test)
+Y_train = scY.fit_transform(Y_train)
+Y_test = scY.transform(Y_test)
 
 #===============================================
 
@@ -42,28 +43,47 @@ from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import GridSearchCV
 
-
+# BUILD NEURAL NETWORK ARCHITECTURE
 def Build_Ann():
     classifier = Sequential()
-    classifier.add(Dense(output_dim=10,init='uniform',activation='relu',input_dim=5))
-    classifier.add(Dense(output_dim=7,init='uniform',activation='relu'))
-    classifier.add(Dense(output_dim=4,init='uniform',activation='relu'))
+    classifier.add(Dense(output_dim=3,init='uniform',activation='relu',input_dim=5))
+    #classifier.add(Dropout(0.2))
+    classifier.add(Dense(output_dim=3,init='uniform',activation='relu'))
+    #classifier.add(Dropout(0.3))
     classifier.add(Dense(output_dim=1,init='uniform',activation='sigmoid'))
     classifier.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
     return classifier
 
-#classifier = KerasClassifier(build_fn=Build_Ann,epochs=500,batch_size=25,verbose=1)
-#accuracies = cross_val_score(estimator=classifier,X=X_train,y=Y_train,n_jobs=1,cv=10)
-#mean=accuracies.mean()
-#variance = accuracies.std()
-##    
-classifier = Build_Ann()
-classifier.fit(X_train,Y_train,batch_size= 10,epochs=1000)
-#X_sampletest = np.array([[41,1,0,2]])
-Y_sampletestpred = classifier.predict(X_test)
-Y_sampletestpred = (Y_sampletestpred>0.2)
 
-#
+
+
+#NETWORK TRAINING
+classifier = Build_Ann()
+classifier.fit(X_train,Y_train,batch_size= 5,epochs=25000)
+
+## SELECT ONE OF THE FOLLOWING:
+
+#CROSS VALIDATION OF STRUCTURE BASED ON INPUT DATA.
+#cross validation on entire data set, producing avg accuracy and variance
+classifier = KerasClassifier(build_fn=Build_Ann,epochs=550,batch_size=22,verbose=1)
+accuracies = cross_val_score(estimator=classifier,X=X_train,y=Y_train,n_jobs=1,cv=10)
+mean=accuracies.mean()
+variance = accuracies.std()
+
+
+
+#SINGLE TEST
+#Single test on 1 data point, input X, it will predict and output Y
+X_sampletest = np.array([[20,1,0,0,4.25]])
+X_sampletest = scX.transform(X_sampletest)
+Y_sampletestpred = classifier.predict(X_sampletest)
+#THRESHOLD SETUP
+Y_sampletestpred = (Y_sampletestpred>0.25)
+
+
+#TEST SET FULL TEST
+#Inputs full X_test dataset into neural network, and produces confusion matrix to evaluate.
+Y_sampletestpred = classifier.predict(X_test)
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(Y_test, Y_sampletestpred)    
 #        
